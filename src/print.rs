b55b1fef72e22788816db69b7c67f6523972ea64
isan <m21029@g.metro-cit.ac.jp>
2024-06-20 11:17:41 +0900

    feat: Implement print to display whole THIR

---
+++ src/print.rs
@@ -0,0 +1,12 @@
+// rustc crates
+use rustc_middle::ty::TyCtxt;
+
+// std crates
+// Own crates
+use crate::thir::thir_tree;
+
+pub fn print_thir<'tcx>(tcx: &TyCtxt<'tcx>) {
+  tcx.mir_keys(()).iter().for_each(|&key| {
+    println!("{}", thir_tree(&tcx, key));
+  });
+}
