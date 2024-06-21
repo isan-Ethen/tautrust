fd54d36b331b98d18d3146891ab6552bdeb41ec9
isan <m21029@g.metro-cit.ac.jp>
2024-06-21 18:38:23 +0900

    feat: Change print function to display reduced THIR. Due to change print function make thir directory and moved printer.rs to there.

--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,4 @@
 // rustc crates
-// use rustc_middle::ty::TyCtxt;
 
 // std crates
 // Own crates
-
-// pub fn print_thir<'tcx>(tcx: TyCtxt<'tcx>) {
-//   tcx.mir_keys(()).iter().for_each(|&key| {
-//     match tcx.thir_body(key) {}
-//     let mut printer = ThirPrinter::new(&thir);
-//     printer.print();
-//     printer.into_buffer();
-//   });
-// }
