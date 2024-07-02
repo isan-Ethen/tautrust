591e82af1d4b3e52061457b541233b91d7abf3ad
isan <m21029@g.metro-cit.ac.jp>
2024-07-02 21:48:04 +0900

    imp: Delete Implementations about lir

--- src/lir.rs
+++ src/lir.rs
@@ -1,10 +1,3 @@
 // rustc crates
 // std crates
 // Own crates
-use crate::thir::RThir;
-mod exprs;
-pub use exprs::*;
-mod lir_generator;
-use lir_generator::get_lir;
-
-pub fn lir<'a, 'tcx>(rthir: &'a RThir<'tcx>) -> Lir<'a, 'tcx> { get_lir(rthir) }
