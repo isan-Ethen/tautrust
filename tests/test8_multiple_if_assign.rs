1daf79ad014d9fccf113268f412b82637776b80b
isan <m21029@g.metro-cit.ac.jp>
2024-07-13 15:09:12 +0900

    feat: Add test files to test if assign expression

--- tests/test8_multiple_if_assign.rs
+++ tests/test8_multiple_if_assign.rs
@@ -4,7 +4,7 @@ fn main() {
     let x = rand_int::<i32>();
     let y = if x >= 0 {
         // x; コレを書くと終わる
-        let n = x + 5;
+        let n = if x >= 5 { x - 5 } else { x };
         t3assert(n >= 0);
         n
     } else {
